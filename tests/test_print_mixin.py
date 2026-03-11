0cfa0fb30d0b9b049a0d1e66ad6dcb4a669199f6
Маргарита Буршева <mbursheva@mail.ru>
2026-03-11 19:40:28 +0300

    adding a new file: tests/test_print_mixin.py

---
+++ tests/test_print_mixin.py
@@ -0,0 +1,17 @@
+from src.product import Product
+from pytest import CaptureFixture
+
+
+def test_print_mixin_on_creation(capsys: CaptureFixture[str]) -> None:
+    """При создании объекта должен автоматически печататься его repr."""
+    Product("Тест", "Описание", 100.0, 5)
+    captured = capsys.readouterr()
+    expected_repr = "Product('Тест', 'Описание', 100.0, 5)"
+    assert expected_repr in captured.out
+
+
+def test_repr_method() -> None:
+    """Проверка метода repr, определённого в PrintMixin."""
+    product = Product("Тест", "Описание", 100.0, 5)
+    expected = "Product('Тест', 'Описание', 100.0, 5)"
+    assert repr(product) == expected
