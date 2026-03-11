0232f2ace9a8a6be496092b34ada185952eeb8f5
Маргарита Буршева <mbursheva@mail.ru>
2026-03-11 19:24:24 +0300

    adding changes to the file: src/product.py

--- src/product.py
+++ src/product.py
@@ -1,23 +1,27 @@
-from typing import Optional, List, Any, Dict
+from typing import Optional, Any, Dict
+
+from mypyc.ir.ops import Sequence
+
+from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
 
 
-class Product:
-    """Класс для представления товаров."""
-
-    name: str
-    description: str
-    __price: float
-    quantity: int
+class Product(BaseProduct, PrintMixin):
+    """Класс для представления товаров(наследник BaseProduct, PrintMixin)."""
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
-        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
-        self.name = name
-        self.description = description
-        self.__price = price
-        self.quantity = quantity
+        """Метод для инициализации экземпляра класса."""
+        # Сначала инициализируем базовый абстрактный класс BaseProduct для установки атрибутов
+        super().__init__(name, description, price, quantity)
+        # Затем явно вызываем миксин для печати
+        PrintMixin.__init__(self)
 
     @classmethod
-    def new_product(cls, product_data: Dict[str, Any], products_list: Optional[List["Product"]] = None) -> "Product":
+    def new_product(
+            cls,
+            product_data: Dict[str, Any],
+            products_list: Optional[Sequence["BaseProduct"]] = None
+    ) -> "BaseProduct":
         """Класс-метод для создания объекта Product из словаря с параметрами. Проверяет наличие товара с таким же
         именем в списке products_list."""
         # Извлекаем параметры из словаря
@@ -48,43 +52,11 @@ class Product:
         new_product = cls(name, description, price, quantity)
         return new_product
 
-    @property
-    def price(self) -> float:
-        """Геттер возвращает текущую цену товара."""
-        return self.__price
-
-    @price.setter
-    def price(self, new_price: float) -> None:
-        """Сеттер устанавливает новую цену товара. Проверяет, что цена положительная. При понижении цены
-        запрашивает подтверждение у пользователя."""
-        # Проверка на нулевую или отрицательную цену
-        if new_price <= 0:
-            print("Цена не должна быть нулевая или отрицательная")
-            return
-
-        # Если цена понижается, запрашиваем подтверждение у пользователя
-        if new_price < self.__price:
-            print(f"Внимание! Цена понижается с {self.__price} до {new_price}.")
-            response = input(
-                "Вы уверены, что хотите понизить цену? Если согласны, введите (y), для отмены " "введите (n): "
-            )
-
-            if response.lower() != "y":
-                print("Изменение цены отменено пользователем.")
-                return
-        # Установка новой цены
-        self.__price = new_price
-        print(f"Установлена цена продукта: {self.__price} руб.")
-
-    def __str__(self) -> str:
-        """Возвращает строку в формате: Название продукта, цена руб. Остаток: количество шт."""
-        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
-
-    def __add__(self, other: "Product") -> float:
+    def __add__(self, other: "BaseProduct") -> float:
         """Возвращает полную стоимость всех товаров на складе. Умножает стоимость и количество всех товаров
         в наличии."""
-        if type(other) is Product:
-            return (self.__price * self.quantity) + (other.__price * other.quantity)
+        if type(self) is type(other):
+            return (self.price * self.quantity) + (other.price * other.quantity)
         else:
             raise TypeError("Ошибка: нельзя сложить объекты разных классов.")
 
