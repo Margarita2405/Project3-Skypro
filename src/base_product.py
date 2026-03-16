57a568754f81395db14d946f80d60d686534c99d
Маргарита Буршева <mbursheva@mail.ru>
2026-03-16 19:42:58 +0300

    adding changes to the file: src/base_product.py

--- src/base_product.py
+++ src/base_product.py
@@ -15,7 +15,10 @@ class BaseProduct(ABC):
         self.name = name
         self.description = description
         self.__price = price
-        self.quantity = quantity
+        if quantity > 0:
+            self.quantity = quantity
+        else:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен.")
 
     @property
     def price(self) -> float:
