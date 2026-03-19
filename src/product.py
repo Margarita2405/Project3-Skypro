fbc9e3c9dcfe6232879cce3163f80f12d81fa147
Маргарита Буршева <mbursheva@mail.ru>
2026-03-19 11:49:57 +0300

    adding changes to the file: src/product.py

--- src/product.py
+++ src/product.py
@@ -10,14 +10,48 @@ class Product(BaseProduct, PrintMixin):
     """Класс для представления товаров(наследник BaseProduct, PrintMixin)."""
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
-        """Метод для инициализации экземпляра класса."""
-        # Проверка нулевого количества до вызова конструктора базового класса
+        """Метод для инициализации экземпляра класса с проверкой количества."""
         if quantity <= 0:
             raise ValueError("Товар с нулевым количеством не может быть добавлен.")
-        # Инициализация базовых атрибутов
-        super().__init__(name, description, price, quantity)
+
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
         # Вызов миксина для печати
         PrintMixin.__init__(self)
+
+    @property
+    def price(self) -> float:
+        """Геттер возвращает текущую цену товара."""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Сеттер устанавливает новую цену товара. Проверяет, что цена положительная.
+        При понижении цены запрашивает подтверждение у пользователя."""
+        # Проверка на нулевую или отрицательную цену
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+
+        # Если цена понижается, запрашиваем подтверждение у пользователя
+        if new_price < self.__price:
+            print(f"Внимание! Цена понижается с {self.__price} до {new_price}.")
+            response = input(
+                "Вы уверены, что хотите понизить цену? Если согласны, введите (y), для отмены " "введите (n): "
+            )
+
+            if response.lower() != "y":
+                print("Изменение цены отменено пользователем.")
+                return
+        # Установка новой цены
+        self.__price = new_price
+        print(f"Установлена цена продукта: {self.__price} руб.")
+
+    def __str__(self) -> str:
+        """Возвращает строковое представление товара в формате: Название продукта, цена руб. Остаток: количество шт."""
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
     @classmethod
     def new_product(
